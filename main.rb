fa86c7a0b275125e3c2c6bf72d528aa5afe7e258
Vincent Langat <104572853+VincentLangat033@users.noreply.github.com>
2022-08-24 16:59:20 +0000

    Initial commit

---
+++ main.rb
@@ -0,0 +1,39 @@
+puts "Hello, World!"
+# p "Welcome to our Ruby session"
+# p [1,2,3,4]
+
+# p "Welcome to our Ruby session1"
+# p "Welcome to our Ruby session2"
+# p "Welcome to our Ruby session3"
+# print "Welcome to my youtube channel!"
+# print "Welcome to my youtube channel!"
+# print "Welcome to my youtube channel!"
+# puts [1,2,3,4]
+# pp [1,2,3,4]
+# pp "Dont forget to like and subscribe!"
+# pp "Dont forget to like and subscribe1!"
+# A puts/p/pp/print has no return value , by default its return value is Nil
+p ["a","b","c","d","e"]
+# def alpha_array
+  alphabet=["a","b","c","d","e"]
+#  puts alphabet.join
+# p alphabet.reverse
+p alphabet.pop
+p alphabet
+p alphabet.push("f")
+p alphabet.shift
+p alphabet
+p alphabet.unshift("Snaida")
+p alphabet
+p alphabet.pop
+p alphabet.slice(1)
+# p alphabet.splice(1)
+p alphabet << "Kabalika"
+p alphabet.sort
+p alphabet.unshift "Snaida"
+p alphabet.uniq
+# puts alphabet.to_chomp
+numbers = [1,3,5,3,6,7,3,1]
+p numbers.max
+p numbers.min
+p numbers.flatten
